0ffb26b6b644225ab2338c44ff2c442255532c75
Lexxythelizard <Lenina.vorbau@posteo.de>
2026-06-12 10:51:47 +0200

    cleaned up del directory

--- del/obj_rm.c
+++ del/obj_rm.c
@@ -24,22 +24,99 @@
 
 // --- prototype ---
 
-int	obj_rm(t_head *head);
+void		*list_strip(t_head *head);
+int			list_rm(t_head *head);
+static void	del_info(t_head *head);
+static void	del_funct(void);
+static void	del_tail(t_head *head);
 
 // --- define ---
 
-int	obj_rm(t_head **head)
+void	*list_strip(t_head *head)
+{
+	void	*strip;
+	t_node	*ptr;
+	int		i;
+
+	if (!head)
+		return (NULL);
+	if (!(head -> info))
+		return (NULL);
+	strip = calloc(((head -> len) + 1) * 8);
+	while ((i < head -> len) && (head -> tail))
+	{
+		ptr = head -> tail -> next;
+		strip[i] = head -> tail -> content;
+		free(head -> tail);
+		head -> tail = ptr;
+		i++;
+	}
+	head -> len = 0;
+	head -> tip = NULL;
+	return (strip);
+}
+
+int	list_rm(t_head **head)
 {
 	int	len;
 
 	if ((!head) || (!(*head)))
 		return (-1);
 	len = head -> len;
-	// call del info
-	// call del tail
-	// call del specifunc
-	// call del basicfunc
-	// call del del
-	// rm object
+	del_info(*head);
+	del_tail(*head);
+	del_func(*head);
+	free(*head);
+	*head = NULL
 	return (len);
+}
+
+static void	del_info(t_head *head)
+{
+	if (!head)
+		return ;
+	if (!(head -> info))
+		return ;
+	head -> del -> del_info(head -> info);
+	head -> info = NULL;
+}
+
+static void	del_tail(t_head *head)
+{
+	t_node	*ptr;
+
+	if (!head)
+		return ;
+	if (!(head -> tail))
+		return ;
+	ptr = head -> tail;
+	while (head -> tail)
+	{
+		ptr = head -> tail -> next;
+		head -> del -> del_content(head -> tail -> content);
+		head -> tail = ptr;
+	}
+	head -> tip = NULL;
+	head - > len = 0;
+}
+
+static void	del_func(t_head *head)
+{
+	if (!head)
+		return ;
+	if (head -> speci)
+	{
+		head -> del -> speci(head -> specifunc);
+		head -> specifunc = NULL;
+	}
+	if (head -> basicfunc)
+	{
+		free(head -> basicfunc);
+		head -> basicfunc = NULL;
+	}
+	if (head -> del)
+	{
+		free(head -> del);
+		head -> del = NULL;
+	}
 }
